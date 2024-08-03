d293077b8f2a9afa2bc63218362cf32370f2149e
@Ash <ashadolphin@gmail.com>
2024-08-03 14:29:23 -0700

    FileFinding Strategy

--- src/main/java/com/myplayground/DesignPatterns/Strategy/stratagies/MinSizeFileFilder.java
+++ src/main/java/com/myplayground/DesignPatterns/Strategy/stratagies/MinSizeFileFilder.java
@@ -1,23 +1,25 @@
 package com.myplayground.DesignPatterns.Strategy.stratagies;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MinSizeFileFilder implements FileStrategy {
+    int minSize;
+    public MinSizeFileFilder(int min) {
+        this.minSize = min;
+    }
     @Override
-    public List<String> find(int size) {
-        System.out.println(" Find all files smaller than size "+ size );
-        return new ArrayList<>();
+    public boolean find(Path file) {
+        System.out.println(" Find all files smaller than size "+ minSize );
+        try {
+            if (Files.size(file) < minSize) return true;
+        } catch (IOException ioException) {
+            System.out.println("Excpetion observed");
+        }
+        return false;
     }
 
-    /*****
-     * NOT USEFUL
-     * @param max
-     * @param min
-     * @return
-     */
-    @Override
-    public List<String> find(int max, int min) {
-        return null;
-    }
 }
