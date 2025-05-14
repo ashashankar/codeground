0610bf4b8696478217bd40a60958edd086b4c779
@Ash <ashadolphin@gmail.com>
2025-05-13 21:01:52 -0700

    cleanedup

--- src/main/java_old/com/myplayground/DesignPatterns/Strategy/stratagies/FileStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.myplayground.DesignPatterns.Strategy.stratagies;
-
-import java.nio.file.Path;
-import java.util.List;
-
-public interface FileStrategy {
-        boolean find(Path fileName);
-
-}
