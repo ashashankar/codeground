5de0e3cdd2d52cbbf46ff4511efde8f8263d0bbe
@Ash <ashadolphin@gmail.com>
2025-09-09 22:06:37 -0700

    Fibonacci 3 implementations Recursion, DP memoization tabulation with space improvements

--- src/main/java/com/myplayground/playground/Fibonacci.java
+++ src/main/java/com/myplayground/playground/Fibonacci.java
@@ -15,9 +15,14 @@ public class Fibonacci {
         Arrays.fill(dp, -1);
         System.out.println(fibonacci_memo(n, dp));
 
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        System.out.println(fibonacci_tabulation(fibonacci_tabulation(n, dp));
+
     }
 
-    //recursion
+    //recursion o(2^n)
     public static int fibonacci(int n) {
         if (n == 0) return 0;
         if(n == 1) return 1;
@@ -26,12 +31,20 @@ public class Fibonacci {
         return x;
     }
 
-    //memoization
+    //memoization time (n) and space - recursion stack + dp array ->o(n + n) ~ o(n)
     public static int fibonacci_memo(int n, int[] dp) {
         if (n == 0) return 0;
         if (n == 1) return 1;
         if(dp[n] != -1) return dp[n];
         dp[n] = fibonacci_memo(n-1, dp) + fibonacci_memo(n-2, dp);
+        return dp[n];
+    }
+
+    //Tabulation -> improved space by eliminating the stack space. time and space o(n).
+    public static int fibonacci_tabulation(int n, int[] dp) {
+        for (int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
     }
 }
