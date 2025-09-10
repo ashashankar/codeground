6cf7d4b3e9447a41500f1de067601ce53e0b0b40
@Ash <ashadolphin@gmail.com>
2025-09-09 21:33:50 -0700

    Fibonacci 3 implementations Recursion, DP memoization tabulation with space improvements

---
+++ src/main/java/com/myplayground/temp/Fibonacci.java
@@ -0,0 +1,17 @@
+package com.myplayground.temp;
+
+/**
+ * This class show fibonacci recursion , memoization and tabulation implemenation.
+ */
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(7));
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0 || n == 1) return 1;
+
+        int x = fibonacci(n-1) + fibonacci(n-2);
+        return x;
+    }
+}
