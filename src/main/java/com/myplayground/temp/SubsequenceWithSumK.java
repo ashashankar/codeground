7a91ba49f11906744a02b614c2b37f32ec411763
@Ash <ashadolphin@gmail.com>
2025-12-03 22:04:38 -0800

    Subsequences with sum K. print all subsequences  print only one subsequence  print count of all subsequence with sum K.

--- src/main/java/com/myplayground/temp/SubsequenceWithSumK.java
+++ src/main/java/com/myplayground/temp/SubsequenceWithSumK.java
@@ -7,7 +7,8 @@ public class SubsequenceWithSumK {
     public static void main(String[] args) {
         int[] arr = {3, 5, 2, 6, 8};
         int k = 8;
-        new SubsequenceWithSumK().printSubequenceWithSumK(arr, 0, 0, k, new ArrayList<Integer>());
+        int result = new SubsequenceWithSumK().countAllSubsequencesWithSumK(arr, 0, 0, k, new ArrayList<Integer>());
+        System.out.println(result);
     }
 
     public void printSubequenceWithSumK(int[] arr, int idx, int sum, int k, List<Integer> values) {
@@ -27,5 +28,51 @@ public class SubsequenceWithSumK {
         values.remove(values.size()-1);
         printSubequenceWithSumK(arr, idx+1,sum,  k, values);
 
+    }
+
+    public boolean printOnlyOneSubequenceWithSumK(int[] arr, int idx, int sum, int k, List<Integer> values) {
+
+        if (idx == arr.length) {
+            if (sum == k) {
+                System.out.println(values);
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+
+        sum += arr[idx];
+        values.add(arr[idx]);
+        boolean found = printOnlyOneSubequenceWithSumK(arr, idx+1,sum,  k, values);
+        if (found) return true;
+
+        sum -= arr[idx];
+        values.remove(values.size()-1);
+        if(printOnlyOneSubequenceWithSumK(arr, idx + 1, sum, k, values)) return true;
+
+        return false;
+    }
+
+    public int countAllSubsequencesWithSumK(int[] arr, int idx, int sum, int k, List<Integer> values) {
+
+        if (idx == arr.length) {
+            if (sum == k) {
+                return 1;
+            } else {
+                return 0;
+            }
+
+        }
+
+        sum += arr[idx];
+        values.add(arr[idx]);
+        int left = countAllSubsequencesWithSumK(arr, idx+1,sum,  k, values);
+
+        sum -= arr[idx];
+        values.remove(values.size()-1);
+        int right = countAllSubsequencesWithSumK(arr, idx + 1, sum, k, values);
+
+        return left + right;
     }
 }
